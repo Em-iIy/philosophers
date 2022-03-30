c5fd99b6ef62e50c95c67a66d020efcecb412b92
Glenn Winnink <gwinnink@student.codam.nl>
2022-03-30 20:33:34 +0200

    added error messages

---
+++ philo/srcs/error_utils.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 15:10:54 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/03/30 17:41:59 by gwinnink         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+static int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static char	*s_cpy(char *ret, const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ret[i] = str[i];
+		i++;
+	}
+	return (&ret[i]);
+}
+
+static char	*add_color(const char *color, const char *msg)
+{
+	char	*ret;
+	int		i;
+	int		j;
+	int		final_len;
+
+	i = 0;
+	j = 0;
+	final_len = ft_strlen(color) + ft_strlen(msg) + 4;
+	ret = (char *)malloc(final_len + 1);
+	if (!ret)
+		return (NULL);
+	ret[final_len] = 0;
+	if (s_cpy(s_cpy(s_cpy(ret, color), msg), NORMAL) - final_len != ret)
+	{
+		free(ret);
+		return (NULL);
+	}
+	return (ret);
+}
+
+int	error_msg(const char *msg)
+{
+	char	*temp;
+
+	temp = add_color(RED, msg);
+	if (!temp)
+		return (2);
+	write(2, temp, ft_strlen(temp));
+	return (1);
+}
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 10:48:54 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/03/29 11:59:08 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/03/30 17:13:49 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ int	main(int argc, char **argv)
 	t_input	input;
 
 	if (argc != 5 && argc != 6)
-		return (1);
+		return (error_msg("error: input: invalid amount of arguments\n\
+Usage: ./philo [number of philosophers] [tt. die] [tt. eat] \
+[tt. sleep] opt. [number of meals]\n"));
 	input = parsing(argc, argv);
 	if (!input.valid)
-		return (1);
+		return (error_msg("error: input: input not valid integer\n"));
 }
