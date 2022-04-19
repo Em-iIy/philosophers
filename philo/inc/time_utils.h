a71094e15c5b4c489a4bb6c4228b71338af10b75
Glenn Winnink <gwinnink@student.codam.nl>
2022-04-19 11:46:37 +0200

    created gettime and usleep functions

---
+++ philo/inc/time_utils.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_utils.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 14:25:58 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/04/12 14:27:11 by gwinnink         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TIME_UTILS_H
+# define TIME_UTILS_H
+
+// ----------------------------------------Prototypes
+unsigned long	get_time(void);
+void			better_usleep(unsigned long n);
+
+#endif
---
+++ philo/srcs/time_utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 14:02:32 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/04/12 14:11:22 by gwinnink         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/time.h>
+#include <unistd.h>
+
+// Returns current time in micro seconds
+unsigned long	get_time(void)
+{
+	struct timeval	t;
+
+	gettimeofday(&t, NULL);
+	return (t.tv_usec + t.tv_sec * 1000000);
+}
+
+void	better_usleep(unsigned long n)
+{
+	unsigned long	start;
+
+	start = get_time();
+	while (n >= get_time() - start)
+		usleep(100);
+}
