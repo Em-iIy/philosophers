c261f8cedeab9f9806a58749cc2e9638289044d9
Glenn Winnink <gwinnink@student.codam.nl>
2022-04-21 18:35:24 +0200

    added death monitor + delay before start

--- philo/srcs/philo_routine.c
+++ philo/srcs/philo_routine.c
@@ -6,28 +6,44 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 16:07:35 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/04/19 10:13:32 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:30:21 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pthread.h>
 #include "philo.h"
 #include "time_utils.h"
+#include <unistd.h>
 
 void	*philo_routine(void *vars)
 {
-	t_philo	philo;
+	t_philo			philo;
+	pthread_t		monitor;
+	unsigned long	last_meal;
 
 	philo = *(t_philo *)vars;
-	while (1)
+	last_meal = *philo.start_time;
+	philo.last_meal = &last_meal;
+	if (pthread_create(&monitor, NULL, &philo_monitor, (void *)&philo) != 0)
+		return (NULL);
+	while (*philo.start_time == 0)
+		usleep(100);
+	if (!(philo.id & 1))
+		usleep(2000);
+	while (!philo.has_died->flag)
 	{
-		printing(philo, "has taken a fork\n");
-		printing(philo, "has taken a fork\n");
-		printing(philo, "is eating\n");
+		pthread_mutex_lock(&(philo.fork1->mtx));
+		printing(philo, NULL, "has taken a fork\n", 0); // error prone
+		pthread_mutex_lock(&(philo.fork2->mtx));
+		printing(philo, NULL, "has taken a fork\n", 0); // error prone
+		printing(philo, &last_meal, "is eating\n", 0); // error prone
 		better_usleep(philo.tt_eat * 1000);
-		printing(philo, "is sleeping\n");
+		pthread_mutex_unlock(&(philo.fork1->mtx));
+		pthread_mutex_unlock(&(philo.fork2->mtx));
+		printing(philo, NULL, "is sleeping\n", 0); // error prone
 		better_usleep(philo.tt_sleep * 1000);
-		printing(philo, "is thinking\n");
+		printing(philo, NULL, "is thinking\n", 0); // error prone
 	}
+	pthread_join(monitor, NULL);
 	return (NULL);
 }
