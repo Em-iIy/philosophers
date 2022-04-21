1ab9be186842b663a4304fc935e78bca5c9a8c97
Glenn Winnink <gwinnink@student.codam.nl>
2022-04-21 18:33:38 +0200

    added calloc

--- philo/srcs/not_libft/ft_calloc.c
+++ philo/srcs/not_libft/ft_calloc.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   not_libft.h                                        :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/13 17:13:26 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/04/19 12:29:34 by gwinnink         ###   ########.fr       */
+/*   Created: 2022/04/19 12:26:58 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/04/19 15:13:12 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef NOT_LIBFT_H
-# define NOT_LIBFT_H
+#include <stdlib.h>
+#include <string.h>
 
-# include <stddef.h>
+void	*ft_calloc(int count, size_t size)
+{
+	char			*ret;
 
-// ----------------------------------------Prototypes
-size_t	ft_strlen(const char *s);
-char	*ft_itoa(int n);
-int		ft_strlcpy(char *dst, const char *src, size_t size);
-void	*ft_calloc(int count, size_t size);
-
-#endif
+	ret = (char *)malloc(count * size);
+	if (!ret)
+		return (NULL);
+	return (memset(ret, 0, count * size));
+}
