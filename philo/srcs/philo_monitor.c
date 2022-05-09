045469df140e7d86902a6251eae92ba1ac9557fc
Glenn Winnink <gwinnink@student.codam.nl>
2022-05-09 19:04:36 +0200

    change check for hunger

--- philo/srcs/philo_monitor.c
+++ philo/srcs/philo_monitor.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 12:52:49 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/04/21 15:30:01 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/05/09 18:44:43 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,34 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdbool.h>
+
+static bool	check_hunger(uint64_t tt_die, uint64_t *last_meal, uint64_t *start_time)
+{
+	if (*last_meal)
+	{
+		if ((get_time() - *last_meal) / 1000 > tt_die)
+			return (false);
+	}
+	else
+	{
+		if ((get_time() - *start_time) / 1000 > tt_die)
+			return (false);
+	}
+	return (true);
+}
 
 void	*philo_monitor(void *vars)
 {
-	t_philo			philo;
-	unsigned long	tt_die;
+	t_philo		philo;
+	uint64_t	tt_die;
 
 	philo = *(t_philo *)vars;
 	tt_die = philo.tt_die;
 	while (*philo.last_meal == 0)
 		usleep(100);
-	while ((get_time() - *philo.last_meal) / 1000 <= tt_die && philo.has_died->flag == 0)
-		usleep(50);
+	while (check_hunger(tt_die, philo.last_meal, philo.start_time) && philo.has_died->flag == 0)
+		usleep(500);
 	if (philo.has_died->flag)
 		return (NULL);
 	pthread_mutex_lock(&(philo.has_died->mtx));
