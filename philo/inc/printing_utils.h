285b3efbb983f8e557e2a0cfa6843d52309758ea
Glenn Winnink <gwinnink@student.codam.nl>
2022-05-10 19:29:24 +0200

    separated printing from philo state

---
+++ philo/inc/printing_utils.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printing_utils.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 12:02:25 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/05/10 12:25:33 by gwinnink         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTING_UTILS_H
+# define PRINTING_UTILS_H
+
+# include "philo.h"
+# include <stdbool.h>
+
+// ----------------------------------------Prototypes
+bool			printing(t_philo philo, int eat, const char *msg);
+
+#endif
