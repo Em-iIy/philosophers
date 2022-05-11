deebb3a4517e4eb5c164d6e9c9f032eb6715c8a6
Glenn Winnink <gwinnink@student.codam.nl>
2022-05-11 18:11:15 +0200

    added timestamp check

--- philo/srcs/time_utils.c
+++ philo/srcs/time_utils.c
@@ -6,12 +6,26 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 14:02:32 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/05/10 16:02:53 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/05/11 16:35:37 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/time.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdint.h>
+
+uint64_t	check_time_stamp(pthread_mutex_t *mtx, uint64_t *num)
+{
+	uint64_t	ret;
+
+	if (pthread_mutex_lock(mtx) != 0)
+		return (0);
+	ret = *num;
+	if (pthread_mutex_unlock(mtx) != 0)
+		return (0);
+	return (ret);
+}
 
 // Returns current time in micro seconds
 uint64_t	get_time(void)
