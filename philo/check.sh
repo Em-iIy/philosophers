bdb918f4c82206e6c6044007fdae7c4324c40674
Glenn Winnink <gwinnink@student.codam.nl>
2022-05-16 18:51:50 +0200

    fixed

--- philo/check.sh
+++ philo/check.sh
@@ -1,10 +1,10 @@
-n=100
-tt_die=210
-tt_eat=100
-tt_sleep=100
+n=200
+tt_die=130
+tt_eat=60
+tt_sleep=60
 make
 ./philo $n $tt_die $tt_eat $tt_sleep > log
-DEAD=`cat log | tail -1 | awk '{print $2}'`
+DEAD=`cat log | tail -1 | awk '{print $3}'`
 cat log | grep " $((($DEAD + n - 2) % n + 1)) "
 cat log | grep " $((($DEAD + n) % n + 1)) "
 cat log | grep " $DEAD "
