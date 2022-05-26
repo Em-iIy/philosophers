df5bb3e62a0af196a42c45bc7f26b763eb525e78
Glenn Winnink <gwinnink@student.codam.nl>
2022-05-26 11:58:17 +0200

    reverted back to 1 cycle function

--- philo/srcs/philo_routine.c
+++ philo/srcs/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 16:07:35 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/05/25 13:33:02 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/05/26 11:54:06 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-static bool	philo_routine_even(t_philo *philo)
-{
-	usleep(2000);
-	while (check_flag(philo->has_died) && !check_flag(&philo->n_meals))
-	{
-		if (grab_fork(philo, philo->fork2) != true)
-			return (false);
-		if (grab_fork(philo, philo->fork1) != true)
-			return (false);
-		if (eat(philo) != true)
-			return (false);
-		if (philo_sleep(philo) != true)
-			return (false);
-		if (think(philo) != true)
-			return (false);
-		if (decrement_flag(&philo->n_meals) != true)
-			return (func_error(philo, "mutex error\n"));
-	}
-	return (true);
-}
-
-static bool	philo_routine_odd(t_philo *philo)
+static bool	philo_cycle(t_philo *philo)
 {
 	while (check_flag(philo->has_died) && !check_flag(&philo->n_meals))
 	{
@@ -69,10 +48,9 @@ void	*philo_routine(void *vars)
 	philo->last_meal = &last_meal;
 	while (!check_time_stamp(&philo->printing->mtx, philo->start_time))
 		usleep(200);
-	if (philo->id & 1)
-		done_eating = philo_routine_odd(philo);
-	else
-		done_eating = philo_routine_even(philo);
+	if (!(philo->id & 1))
+		usleep(2000);
+	done_eating = philo_cycle(philo);
 	if (done_eating)
 		if (decrement_flag(philo->done_eating) != true)
 			func_error(philo, "mutex error\n");
